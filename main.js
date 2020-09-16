e48fd2949c0b250d457e2f8faff150788660f7ab
Marzeu <marzahell@hotmail.com>
2020-09-16 18:17:21 -0300

    exemplos de classes

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-alert('Teste');
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'narza';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo todo2');
+}
+
+MinhaLista.mostraUsuario();
