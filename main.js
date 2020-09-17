bc63cf6ef7a0dcc37321e92d6e9566d8d3ed5e05
Marzeu <marzahell@hotmail.com>
2020-09-17 17:43:22 -0300

    webpack instalado

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+import { soma } from './funcoes';
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
-
-class TodoList extends List {
-    constructor() {
-        super();
-
-        this.usuario = 'narza';
-    }
-
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
-}
-
-const MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function () {
-    MinhaLista.add('Novo todo2');
-}
-
-MinhaLista.mostraUsuario();
+console.log(soma(1, 2));
