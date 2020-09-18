7e0e2beae80442cb1ee911a0b52692255e9eb90c
Marzeu <marzahell@hotmail.com>
2020-09-18 03:02:10 -0300

    tratamento de erro e função loading

--- src/main.js
+++ src/main.js
@@ -15,6 +15,20 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+
+        } else {
+            document.getElementById('loading').remove();
+
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -23,22 +37,31 @@ class App {
         if (repoInput.length === 0)
             return;
 
-        const response = await api.get(`/repos/${repoInput}`);
+        this.setLoading();
 
-        const { name, description, html_url, owner: { avatar_url } } = response.data
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
 
-        console.log(response);
+            const { name, description, html_url, owner: { avatar_url } } = response.data
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        });
+            console.log(response);
 
-        this.inputEl.value = '';
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
 
-        this.render();
+            this.inputEl.value = '';
+
+            this.render();
+
+        } catch (err) {
+            alert('O repositorio não existe!');
+        }
+
+        this.setLoading(false);
     }
 
     render() {
