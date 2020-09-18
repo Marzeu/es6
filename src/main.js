4904da4cb2d58457c525bb069981c036feae5342
Marzeu <marzahell@hotmail.com>
2020-09-18 02:50:38 -0300

    conexão com a api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import api from './api';
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -11,18 +15,60 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        const { name, description, html_url, owner: { avatar_url } } = response.data
+
+        console.log(response);
+
         this.repositories.push({
-            name: 'Marza',
-            description: 'Muito gato',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/51306062?s=400&u=326999fd00b8f9f5329d69eb579580d080179818&v=4',
-            html_url: 'https://github.com/Marzeu?tab=repositories',
+            name,
+            description,
+            avatar_url,
+            html_url,
         });
 
-        console.log(this.repositories);
+        this.inputEl.value = '';
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-new App();
+new App(); 
